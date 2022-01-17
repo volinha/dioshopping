28ebeb24f634ac8b1d174d79bfc2346b6246c358
Vinícius Fernandes <viniciusfernandesmct@gmail.com>
2022-01-17 00:58:43 -0300

    removed: container

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './components/store';
 import Routes from './routes';
-import { Container } from '@material-ui/core/';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 
 const App = () => {
   
@@ -15,12 +14,10 @@ const App = () => {
   
   return(
     <Provider store={store}>
-      <Container maxWidth="xl">
         <Router>
           <Header />
           <Routes />
         </Router>
-      </Container> 
     </Provider>
   )
 }
